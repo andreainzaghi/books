e58c07a15ce50df095c9bece937748782d1a4fc6
Andrea <andrea3aprile99@gmail.com>
2021-06-25 15:53:10 +0200

    mod

--- js/imagebook.js
+++ js/imagebook.js
@@ -9,72 +9,94 @@ new Vue({
     },
     {
      image: { backgroundImage: "https://th.bing.com/th/id/OIP.8TD_d_dRAQZ9nMWBjjB8pwHaLe?w=182&h=283&c=7&o=5&dpr=1.93&pid=1.7" },
+      title:'Le cronache di Narnia'
     },
     {
      image: { backgroundImage: "url(https://th.bing.com/th/id/OIP.D3CDRZ9aGQqoi6h9j5rPRAHaGF?pid=ImgDet&rs=1)" },
     },
     {
      image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+        title:'Le cronache di Narnia'
     },
     {
      image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+        title:'Le cronache di Narnia'
     },
     {
      image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+        title:'Le cronache di Narnia'
     },
     {
      image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+        title:'Le cronache di Narnia'
     },
     {
      image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+        title:'Le cronache di Narnia'
     },
     {
      image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+        title:'Le cronache di Narnia'
     },
     {
      image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+        title:'Le cronache di Narnia'
     },
     {
      image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+        title:'Le cronache di Narnia'
     },
     {
      image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+        title:'Le cronache di Narnia'
     },
     {
      image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+        title:'Le cronache di Narnia'
     },
     {
      image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+        title:'Le cronache di Narnia'
     },
     {
      image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+        title:'Le cronache di Narnia'
     },
     {
      image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+        title:'Le cronache di Narnia'
     },
     {
      image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+        title:'Le cronache di Narnia'
     },
     {
      image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+        title:'Le cronache di Narnia'
     },
     {
      image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+        title:'Le cronache di Narnia'
     },
     {
      image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+        title:'Le cronache di Narnia'
     },
     {
      image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+        title:'Le cronache di Narnia'
     },
     {
      image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+        title:'Le cronache di Narnia'
     },
     {
      image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+        title:'Le cronache di Narnia'
     },
     {
      image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+        title:'Le cronache di Narnia'
     }]
 
   },
