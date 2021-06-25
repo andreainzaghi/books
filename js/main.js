5cb38246179477e2387400ff62eb1ddf691197bf
Andrea <andrea3aprile99@gmail.com>
2021-06-25 14:38:04 +0200

    overlay

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 window.addEventListener("scroll",function(){
   var header = document.querySelector("header");
+  const hero = document.querySelector('.overlayblack');
   header.classList.toggle("sticky",window.scrollY > 0);
+  hero.classList.toggle("overlay",window.scrollY > 0);
 })
--- js/search.js
+++ js/search.js
@@ -1,6 +1,7 @@
 
 const icon = document.querySelector('.icon');
 const search = document.querySelector('.search');
+
 icon.onclick = function(){
   search.classList.toggle('active')
 }
