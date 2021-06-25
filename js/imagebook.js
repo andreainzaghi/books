716303d5ad562fba15b10487a0794ed3730ba7a4
Andrea <andrea3aprile99@gmail.com>
2021-06-25 12:44:08 +0200

    3

---
+++ js/imagebook.js
@@ -0,0 +1,90 @@
+
+new Vue({
+  el: '#root',
+  data: {
+
+    books:[ {
+     image: { backgroundImage: "url(https://th.bing.com/th/id/R8f58158c67fd848b1f6c8a33b13f66a0?rik=JuAS9Ybj4es0Nw&pid=ImgRaw)" },
+       title:'Le cronache di Narnia'
+    },
+    {
+     image: { backgroundImage: "https://th.bing.com/th/id/OIP.8TD_d_dRAQZ9nMWBjjB8pwHaLe?w=182&h=283&c=7&o=5&dpr=1.93&pid=1.7" },
+    },
+    {
+     image: { backgroundImage: "url(https://th.bing.com/th/id/OIP.D3CDRZ9aGQqoi6h9j5rPRAHaGF?pid=ImgDet&rs=1)" },
+    },
+    {
+     image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+    },
+    {
+     image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+    },
+    {
+     image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+    },
+    {
+     image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+    },
+    {
+     image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+    },
+    {
+     image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+    },
+    {
+     image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+    },
+    {
+     image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+    },
+    {
+     image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+    },
+    {
+     image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+    },
+    {
+     image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+    },
+    {
+     image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+    },
+    {
+     image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+    },
+    {
+     image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+    },
+    {
+     image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+    },
+    {
+     image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+    },
+    {
+     image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+    },
+    {
+     image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+    },
+    {
+     image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+    },
+    {
+     image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+    },
+    {
+     image: { backgroundImage: "url(https://vuejs.org/images/logo.png)" },
+    }]
+
+  },
+  computed: {
+
+  },
+  methods:{
+
+
+  }
+
+
+});
