70697c73a63a353ac0fe867cc5469d49cb2aeeee
Andrea <andrea3aprile99@gmail.com>
2021-06-26 10:38:46 +0200

    scroll

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 window.addEventListener("scroll",function(){
   var header = document.querySelector("header");
-    // var chart1 = document.querySelector(".chart");
+    header.classList.toggle("sticky",window.scrollY > 0);
+
   const hero = document.querySelector('.overlayblack');
-  header.classList.toggle("sticky",window.scrollY > 0);
   hero.classList.toggle("overlay",window.scrollY > 0);
+
+    // var chart1 = document.querySelector(".chart");
   // chart1.classList.toggle("bchartb",window.scrollY > 0);
+  //
+  // const herowhite = document.querySelector('.btn-overlay');
+  // herowhite.classList.toggle("overlaybutton",window.scrollY < 5);
 
 })
--- js/scrollb.js
+++ js/scrollb.js
@@ -1,14 +1,15 @@
 let progress = document.getElementById('progressbar');
 let totalHeight = document.body.scrollHeight - window.innerHeight;
 window.onscroll = function(){
-  let progressHeight = (window.pageYOffset/totalHeight)*100;
+  var progressHeight = (window.pageYOffset/totalHeight)*100;
 
   progress.style.height = progressHeight + "%";
-  console.log(progressHeight);
+
+  // if (progressHeight < 95) {
+  //    document.getElementById("btn-overlay").classList.toggle("overlaybutton");
+  // }
 if (progressHeight<101) {
     document.getElementById('progressHeight').innerHTML = progressHeight.toFixed();
 }
-
-
 
 }
