3093de9eb91df6abd4b9985bc3ffd3c2fe3524c3
Andrea <andrea3aprile99@gmail.com>
2021-06-26 14:37:36 +0200

    copertine

--- js/imagebook.js
+++ js/imagebook.js
@@ -5,7 +5,7 @@ new Vue({
 
     books:[ {
      image: { backgroundImage: "url(https://th.bing.com/th/id/OIP.mGtfLytIUT-A5jMpXVzQfgAAAA?pid=ImgDet&rs=1)" },
-         title:'Le cronache di Narnia',
+         title:'La Lettera',
      like:'45'
 
     },
@@ -16,128 +16,128 @@ new Vue({
     },
     {
      image: { backgroundImage: "url(https://th.bing.com/th/id/Ra1d9a2076ec7989f0bb67665f4c31b82?rik=HYaWIfhd3cbiwA&pid=ImgRaw)" },
-         title:'Le cronache di Narnia',
+         title:'Il vecchio e il Mare',
            like:'145'
     },
     {
      image: { backgroundImage: "url(https://i.pinimg.com/originals/49/6e/be/496ebe206fa0105df7f56ba42f1bfb45.jpg)" },
-        title:'Le cronache di Narnia',
+        title:'Il blu dipinto di blu',
         like:'67'
     },
     {
      image: { backgroundImage: "url(https://th.bing.com/th/id/R78876e362c30a446983ff406ebf23b59?rik=9x5Q6bmJ2TUasQ&riu=http%3a%2f%2fprodimage.images-bn.com%2fpimages%2f9780470503607_p0_v1_s1200x630.jpg&ehk=VNHaFd6G3sCRLqEuqQKI8OeUhd02ehAU67Mkz1TTyVQ%3d&risl=&pid=ImgRaw)" },
-        title:'Le cronache di Narnia',
+        title:'Contare i secondi',
         like:'98'
     },
     {
      image: { backgroundImage: "url(https://th.bing.com/th/id/OIP.ZKZufRL0AAlfdJ1ddiIgYgAAAA?pid=ImgDet&rs=1)" },
-        title:'Le cronache di Narnia',
+        title:'Medical disorder',
         like:'134'
     },
     {
      image: { backgroundImage: "url(https://www.textbooknbeyond.com/images/Book_Supplemental_8/9780007800803.jpg)" },
-        title:'Le cronache di Narnia',
+        title:'Inglese per Pricipianti',
         like:'56'
     },
     {
      image: { backgroundImage: "url(https://th.bing.com/th/id/Rceae5db6bcaf2d360a94d5932b981ae4?rik=IjA7Sx7HXKt%2fmA&riu=http%3a%2f%2fimg.libreriadelgiurista.it%2fbooks%2fy%2fYXQQGRYq3y9C-mb.jpg&ehk=zsxAKeizyHVNyS4wwcEpmzxDiInYnCmV%2b6VaSEbrkbc%3d&risl=&pid=ImgRaw)" },
-        title:'Le cronache di Narnia',
+        title:'Freud',
 
       like:'89'
     },
     {
      image: { backgroundImage: "url(https://image.isu.pub/130409135922-6fc44b8c929c403d9c23d3f90f49158e/jpg/page_1_thumb_large.jpg)" },
-        title:'Le cronache di Narnia',
+        title:'La storia di quasi tutto',
 
       like:'34'
     },
     {
      image: { backgroundImage: "url(https://th.bing.com/th/id/OIP.lASvjXmO_cN3mGX8bcHAjgAAAA?pid=ImgDet&w=353&h=500&rs=1)" },
-        title:'Le cronache di Narnia',
+        title:'Javascript',
 
       like:'89'
     },
     {
      image: { backgroundImage: "url(https://image.isu.pub/151116102654-18c392a1700edbfb59c5f9fef75c534a/jpg/page_1_thumb_large.jpg)" },
-        title:'Le cronache di Narnia',
+        title:'HTML5&CSS3',
 
       like:'112'
     },
     {
      image: { backgroundImage: "url(https://is3-ssl.mzstatic.com/image/thumb/Purple123/v4/bd/84/ca/bd84caee-8dd4-989f-68f0-1bf7fbeb664b/mzl.zwkhmfal.png/750x750bb.jpeg)" },
-        title:'Le cronache di Narnia',
+        title:'Come iniziare a viaggiare',
 
       like:'99'
     },
     {
      image: { backgroundImage: "url(https://www.editorialteseo.com/wp-content/uploads/2018/03/68e329f6a91600a21dd8a37b247ca1f5-600x923.jpg)" },
-        title:'Le cronache di Narnia',
+        title:'Trovare Lavoro ai Caraibi',
 
       like:'48'
     },
     {
      image: { backgroundImage: "url(https://th.bing.com/th/id/OIP.hwDalvSSZTZw89E0lnwjLQAAAA?pid=ImgDet&w=200&h=284&rs=1)" },
-        title:'Le cronache di Narnia',
+        title:'Se solo non fossi...',
 
       like:'85'
     },
     {
      image: { backgroundImage: "url(https://products-images.di-static.com/image/sodis-refus-de-soins-et-actualites-sur-les-droits-des-malades/9782810900664-475x500-1.jpg)" },
-        title:'Le cronache di Narnia',
+        title:'Come cavalcare i cavalli',
 
       like:'39'
     },
     {
      image: { backgroundImage: "url(https://th.bing.com/th/id/OIP.KGTBg-CLppi5bZ551vo6ngHaK_?pid=ImgDet&w=480&h=712&rs=1)" },
-        title:'Le cronache di Narnia',
+        title:'12 racconti',
 
       like:'231'
     },
     {
      image: { backgroundImage: "url(https://images.booklooker.de/x/00Surd/Hugo-Brodbeck-Aebi+Einf%C3%BChrung-in-die-praktische-Biochemie-F%C3%BCr-Studierende-der-Medizin.jpg)" },
-        title:'Le cronache di Narnia',
+        title:'Peccato che non siamo assieme',
 
       like:'45'
     },
     {
      image: { backgroundImage: "url(https://images2.medimops.eu/product/ce9baf/M03410248331-source.jpg)" },
-        title:'Le cronache di Narnia',
+        title:'E ora?',
 
       like:'57'
     },
     {
      image: { backgroundImage: "url(https://www.afd.de/wp-content/uploads/sites/111/2021/01/cover_grundsatzprogramm.png)" },
-        title:'Le cronache di Narnia',
+        title:'Azzurro?,no Blu',
 
       like:'85'
     },
     {
      image: { backgroundImage: "url(https://th.bing.com/th/id/OIP.xEVCqiEM9HzK3yugDx_lcAHaKp?pid=ImgDet&w=555&h=798&rs=1)" },
-        title:'Le cronache di Narnia',
+        title:'Neri una storia di sangue',
 
       like:'65'
     },
     {
      image: { backgroundImage: "url(https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/lrg/9780/1943/9780194392365.jpg)" },
-        title:'Le cronache di Narnia',
+        title:'Programming',
 
       like:'89'
     },
     {
      image: { backgroundImage: "url(https://th.bing.com/th/id/OIP.IYU_PiYzgkgGthbEvQshjgAAAA?pid=ImgDet&rs=1)" },
-        title:'Le cronache di Narnia',
+        title:'PHP&SQL',
 
       like:'75'
     },
     {
      image: { backgroundImage: "url(https://media.springernature.com/w306/springer-static/cover/book/978-3-658-13082-4.jpg)" },
-        title:'Le cronache di Narnia',
+        title:'Come scrivere un libro',
 
       like:'23'
     },
     {
      image: { backgroundImage: "url(https://image.isu.pub/120605120102-32b2c297d1794f55bb1a40fa13040edb/jpg/page_1.jpg)" },
-        title:'Le cronache di Narnia',
+        title:'Perche Dio esiste',
 
       like:'12'
     }]
